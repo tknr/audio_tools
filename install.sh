0da8198663d8e63b240c794b26ba069c9bf9bf05
tknr <geishagirlskoume@gmail.com>
2021-11-18 19:23:30 +0900

    new file:   filename_to_mp3_tag 	modified:   install.sh 	renamed:    rename_mp3_by_tag -> mp3_tag_to_filename

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ rm /usr/local/bin/upconvert_mp3
 ln -s `pwd`/upconvert_mp3 /usr/local/bin/
 rm /usr/local/bin/add_cover_mp3
 ln -s `pwd`/add_cover_mp3 /usr/local/bin/
-rm /usr/local/bin/rename_mp3_by_tag
-ln -s `pwd`/rename_mp3_by_tag /usr/local/bin/
+rm /usr/local/bin/mp3_tag_to_filename
+ln -s `pwd`/mp3_tag_to_filename /usr/local/bin/
+rm /usr/local/bin/filename_to_mp3_tag
+ln -s `pwd`/filename_to_mp3_tag /usr/local/bin/
 
 
