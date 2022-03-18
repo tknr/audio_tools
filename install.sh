3e79cb9941eb69c7edfe4682fe3403fe82246548
tknr <geishagirlskoume@gmail.com>
2022-03-18 12:34:55 +0900

    u

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ rm /usr/local/bin/mp3_tag_to_filename
 ln -s `pwd`/mp3_tag_to_filename /usr/local/bin/
 rm /usr/local/bin/filename_to_mp3_tag
 ln -s `pwd`/filename_to_mp3_tag /usr/local/bin/
-
+rm /usr/local/bin/export_artwork_mp3
+ln -s `pwd`/export_artwork_mp3 /usr/local/bin/
 
